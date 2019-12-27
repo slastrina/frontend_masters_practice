551672b80b000deda0de3b5527e292fa4788a94f
Samuel Lastrina <50973822+slastrina@users.noreply.github.com>
2019-12-27 14:51:00 +1100

    convert app.js to jsx

--- intro_to_react_v5/adopt-me/src/Pet.js
+++ intro_to_react_v5/adopt-me/src/Pet.js
@@ -1,17 +1,11 @@
 import React from "react";
 
 export default function Pet({ name, animal, breed }) {
-// React Pattern
-  // return React.createElement("div", {}, [
-  //   React.createElement("h1", {}, name),
-  //   React.createElement("h2", {}, animal),
-  //   React.createElement("h2", {}, breed)
-  // ]);
+  const x = name.toUpperCase(name)
 
-// jsx pattern
   return (
     <div>
-      <h1>{name}</h1>
+      <h1>Name: {x}</h1>
       <h2>{animal}</h2>
       <h2>{breed}</h2>
     </div>
