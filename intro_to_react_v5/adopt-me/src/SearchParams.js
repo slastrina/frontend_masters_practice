1cdcc9acb4a337cc38451068a0bcbacc5e69bb2d
Samuel Lastrina <50973822+slastrina@users.noreply.github.com>
2019-12-27 16:05:58 +1100

    adding animal and breed to form

--- intro_to_react_v5/adopt-me/src/SearchParams.js
+++ intro_to_react_v5/adopt-me/src/SearchParams.js
@@ -1,7 +1,11 @@
 import React, {useState} from "react";
+import {ANIMALS} from "@frontendmasters/pet";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("dog");
+  const [breed, setBreed] = useState("");
+  const [breeds, setBreeds] = useState([]);
 
   return (
     <div className="search-params">
@@ -12,6 +16,27 @@ const SearchParams = () => {
                  value={location}
                  placeholder="location"
                  onChange={event => setLocation(event.target.value)}/>
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select id="animal"
+                 value={animal}
+                 onBlur={event => setAnimal(event.target.value)}
+                  onChange={event => setAnimal(event.target.value)}>
+            <option>All</option>
+            {ANIMALS.map(animal => (<option key={animal} value={animal}>{animal}</option>))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Animal
+          <select id="breed"
+                  value={breed}
+                  onBlur={event => setBreed(event.target.value)}
+                  onChange={event => setBreed(event.target.value)}
+                  disabled={breeds.length === 0}>
+            <option>All</option>
+            {breeds.map(breedString => ( <option key={breedString} value={breedString}>{breedString}</option> ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
